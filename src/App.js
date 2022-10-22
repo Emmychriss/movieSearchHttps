b0f5fbe25124b601a659d51118aa61085318f0a6
Emmanuel <emmychris477@gmail.com>
2022-10-22 21:13:32 +0100

    async await for fetch on SWAPI

--- src/App.js
+++ src/App.js
@@ -6,22 +6,18 @@ import "./App.css";
 const App = () => {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((moviedata) => {
-          return {
-            id: moviedata.episode_id,
-            title: moviedata.title,
-            releaseDate: moviedata.release_date,
-            openingText: moviedata.opening_crawl,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    const transformedMovies = data.results.map((moviedata) => {
+      return {
+        id: moviedata.episode_id,
+        title: moviedata.title,
+        releaseDate: moviedata.release_date,
+        openingText: moviedata.opening_crawl,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
